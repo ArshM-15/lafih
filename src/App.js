7b1aa79382f18042c989ffdf90f08248c2b49a9e
Arshvir Mundi <arshvir.mundi@gmail.com>
2023-04-02 20:06:46 -0400

    c

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
     <div className="App">
       <Routes>
         <Route exact path="/" element={<Login />} />
-        <Route exact path="/home/:uid" element={<Home />} />
+        <Route exact path="/home/" element={<Home />} />
       </Routes>
     </div>
   );
