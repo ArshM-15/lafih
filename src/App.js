fe483cffe87ec3d728ae87623cba02395c4c7a83
Arshvir Mundi <arshvir.mundi@gmail.com>
2023-04-09 17:44:31 -0400

    ..

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import "./App.css";
-import { Routes, Route } from "react-router-dom";
+import { Switch, Route } from "react-router-dom";
 import Home from "./components/home/home";
 import Login from "./components/login/login";
 
 function App() {
   return (
     <div className="App">
-      <Routes>
+      <Switch>
         <Route path="/" element={<Login />} />
         <Route path="/home/:uid" element={<Home />} />
-      </Routes>
+      </Switch>
     </div>
   );
 }
