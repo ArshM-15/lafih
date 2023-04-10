53e9ea7f2b12a85e33b6604cd7714260f78f46e0
Arshvir Mundi <arshvir.mundi@gmail.com>
2023-04-09 23:38:08 -0400

    ..

--- src/App.js
+++ src/App.js
@@ -1,24 +1,5 @@
-// import "./App.css";
-// import { Route, Routes, BrowserRouter, Switch } from "react-router-dom";
-// import Home from "./components/home/home.jsx";
-// import Login from "./components/login/login.jsx";
-
-// function App() {
-//   return (
-//     <div className="App">
-//       <Routes>
-//         <Route exact path="/home/:uid" element={<Home />} />
-//         <Route exact path="/" element={<Login />} />
-//       </Routes>
-//     </div>
-
-//   );
-// }
-
-// export default App;
-
 import "./App.css";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Route, Routes, BrowserRouter, Switch } from "react-router-dom";
 import Home from "./components/home/home.jsx";
 import Login from "./components/login/login.jsx";
 
