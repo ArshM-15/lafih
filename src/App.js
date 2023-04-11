687c5dac840891e08c42dbac40cb5ed0ba2cd296
Arshvir Mundi <arshvir.mundi@gmail.com>
2023-04-10 20:27:11 -0400

    ..

--- src/App.js
+++ src/App.js
@@ -1,9 +1,33 @@
-import "./App.css";
-import { Route, Routes, BrowserRouter, Switch } from "react-router-dom";
+// import "./App.css";
+// import { Route, Routes, BrowserRouter, Switch } from "react-router-dom";
+// import Home from "./components/home/home.jsx";
+// import Login from "./components/login/login.jsx";
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <Routes>
+//         <Route exact path="/home/:uid" element={<Home />} />
+//         <Route exact path="/" element={<Login />} />
+//       </Routes>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+import React, { useEffect } from "react";
+import { Route, Routes } from "react-router-dom";
 import Home from "./components/home/home.jsx";
 import Login from "./components/login/login.jsx";
+import ReactGA from "react-ga";
 
 function App() {
+  useEffect(() => {
+    ReactGA.initialize("G-CMSWN17LP0");
+    ReactGA.pageview(window.location.pathname + window.location.search);
+  }, []);
+
   return (
     <div className="App">
       <Routes>
